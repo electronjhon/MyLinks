e7f5ee016a2560614d4aa07f5edd3d4a16809ad0
Electronjhon <63406986+electronjhon@users.noreply.github.com>
2020-06-13 10:15:00 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -3,7 +3,7 @@ module.exports = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: 'Angeljhon123*',
         database: 'database_links'
     }
 }
